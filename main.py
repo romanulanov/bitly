259a5e76aecf17f1da4755b41425b0bc4b722a05
pomsk10 <turnsourcream@gmail.com>
2023-05-13 20:39:31 +0400

    Уточнил название переменной

--- main.py
+++ main.py
@@ -8,42 +8,43 @@ from dotenv import load_dotenv
 URL = "https://api-ssl.bitly.com/v4/"
 
 
-def is_bitlink(token, url):
+def is_bitlink(header, url):
     response = requests.get(
-        "{}{}{}".format(URL, "bitlinks/", url), headers={"Authorization": token})
+        "{}{}{}".format(URL, "bitlinks/", url), headers=header)
     return response.ok
 
 
-def shorten_link(token, url):
-    response = requests.post("{}{}".format(URL, "shorten"), headers={"Authorization": token}, json={'long_url': url})
+def shorten_link(header, url):
+    response = requests.post("{}{}".format(URL, "shorten"), headers=header, json={'long_url': url})
     response.raise_for_status()
     bitlink = response.json()["link"]
     return bitlink
 
 
-def count_clicks(token, url):
-    total_clicks = requests.get("{}{}{}{}".format(URL, "bitlinks/", url, "/clicks/summary"), headers={"Authorization": token})
+def count_clicks(header, url):
+    total_clicks = requests.get("{}{}{}{}".format(URL, "bitlinks/", url, "/clicks/summary"), headers=header)
     total_clicks.raise_for_status()
     return total_clicks.json()['total_clicks']
 
 
 def main():
     load_dotenv()
-    token = "Bearer " + os.environ["BITLY_TOKEN"]
+    token = os.environ["BITLY_TOKEN"]
+    header = {"Authorization": token}
     parser = argparse.ArgumentParser()
     parser.add_argument ('url', nargs='?')
     args = parser.parse_args()
     full_url = urlparse(args.url.strip())
     url = "{}{}".format(full_url.netloc, full_url.path)
-    if is_bitlink(token, url):
+    if is_bitlink(header, url):
         try:
-            total_clicks = count_clicks(token, url)
+            total_clicks = count_clicks(header, url)
             print("Количество кликов: {}".format(total_clicks))
         except requests.exceptions.HTTPError:
             print("Ошибка при вводе битлинка!")
     else:
         try:
-            bitlink = shorten_link(token, full_url.geturl())
+            bitlink = shorten_link(header, full_url.geturl())
             print("Битлинк: {}".format(bitlink))
         except requests.exceptions.HTTPError:
             print("Ошибка при вводе ссылки!")
